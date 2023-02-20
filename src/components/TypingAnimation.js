9c7973c507dcc51040d982d26307b75d234dca7f
Lucas <lucass706@hotmail.com>
2023-02-20 17:05:39 -0300

    add project template

---
+++ src/components/TypingAnimation.js
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
+
+const TypingAnimation = ({ data }) => {
+  // Create Ref element.
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: data
+        ? data
+        : ["Software Engineer", "Skils ->", "Javascript", "React", "NodeJs", "Typescript"], // Strings to display
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 100,
+      smartBackspace: true,
+      loop: true,
+      showCursor: false,
+    });
+
+    // Destropying
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+  return <p ref={el}></p>;
+};
+export default TypingAnimation;
